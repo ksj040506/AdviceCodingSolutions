8d4ade6966f00f6248399763e70ec8d46c758063
ksj040506 <ksj040506@naver.com>
2025-10-04 22:51:43 +0900

    [Bronze III] Title: 공 넣기, Time: 216 ms, Memory: 18660 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+      int[] arr = new int[n];
+
+      for(int i = 0; i < m ; i++ ) {
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        int num = sc.nextInt();
+        for(int j = start-1; j < end; j++ ){
+          arr[j] = num;
+        }
+      }
+      for(int i : arr) System.out.print(i + " ");
+
+  }     
+}
