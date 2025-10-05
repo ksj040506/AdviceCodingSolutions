1e7c43ca601f8db37ea636e62cf41513d5bf2ca0
ksj040506 <ksj040506@naver.com>
2025-10-05 22:40:27 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 168 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,21 +1,23 @@
-import java.util.Scanner;
-import java.util.HashSet;
+import java.util.*;
 public class Main {
     public static void main(String[] args){
       Scanner sc = new Scanner(System.in);
       HashSet<Integer> set = new HashSet<>();
-      int[] arr = new int[28];
 
       for(int i = 0; i < 30; i++) {
         set.add(i+1);
       }
       
       for(int i = 0; i < 28; i++) {
-        arr[i] = sc.nextInt();
-        set.remove(arr[i]);
+        set.remove(sc.nextInt());
       }
       
-      for(int i : set) {
+      List<Integer> list = new ArrayList<>(set);
+
+      Collections.sort(list);
+    
+
+      for(int i : list) {
         System.out.println(i);
       }    
   }     
