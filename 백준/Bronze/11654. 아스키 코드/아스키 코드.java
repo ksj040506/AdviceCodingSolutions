38611bf37cdede14eb101373a479ca98d0d4642f
ksj040506 <ksj040506@naver.com>
2025-10-09 01:03:57 +0900

    [Bronze V] Title: 아스키 코드, Time: 168 ms, Memory: 17580 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -2,10 +2,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args){
       Scanner sc = new Scanner(System.in);
-      String str  = sc.nextLine();
-      char[] ch = str.toCharArray();
-      
-      for(int i : ch) System.out.println(i);
+      char ch  = sc.next().charAt(0);
+      System.out.println((int)ch);
 
   }     
 }
