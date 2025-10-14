3ade02b29aa752014e358aff764e17ebfd0a6689
ksj040506 <ksj040506@naver.com>
2025-10-14 21:42:05 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 180 ms, Memory: 17840 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+
+      for(int i = 1; i <= n; i++) {
+        System.out.println(" ".repeat(n-i) + "*".repeat(i*2-1));
+      }
+      for(int i = n-1; i > 0; i--) {
+        System.out.println(" ".repeat(n-i) + "*".repeat(i*2-1));
+      }
+  }  
+}
+
+
+
