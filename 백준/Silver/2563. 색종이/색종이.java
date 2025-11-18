1ad316238ec17f71328493c7aad98a782919bd5b
ksj040506 <ksj040506@naver.com>
2025-11-19 04:15:49 +0900

    [Silver V] Title: 색종이, Time: 176 ms, Memory: 17928 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -3,12 +3,14 @@ public class Main {
     public static void main(String[] args){
       Scanner sc = new Scanner(System.in);
       boolean [][] map = new boolean[100][100];
+      
       int n = sc.nextInt();
       int count = 0;
 
       for (int i = 0; i < n; i++) {
         int x = sc.nextInt();
         int y = sc.nextInt();
+
         for (int j = 0; j < 10; j++) {
           for (int k = 0; k < 10; k++) {
             map[x+j][y+k] = true;
@@ -18,15 +20,11 @@ public class Main {
 
       for(int x = 0; x < 100; x++) {
         for(int y = 0; y < 100; y++) {
-          if(map[x][y] == true) {
-            count++;
-          }
+          if(map[x][y]) count++;
         }
       }
 
       System.out.println(count);
-
-   
     }
   }  
 
