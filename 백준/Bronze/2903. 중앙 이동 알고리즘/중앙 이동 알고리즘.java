d8bc6788ade8c20ecf0567adaee54c6c33852551
ksj040506 <ksj040506@naver.com>
2025-11-28 21:00:39 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 172 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -1,11 +1,12 @@
 import java.util.*;
 public class Main {
 // 초기 4개 점(사각형 1개) -> 9개(사각형4개) -> 25개(사각형16개-> 루트씌우고 1더해서 제곱) -> 81(64개) -> ...-> 1089(1024개->루트씌우고 1더하고 제곱)
+// ((4^n)^1/2 + 1)^2= (2^n + 1)^2
   public static void main(String[] args){
    Scanner sc = new Scanner(System.in);
    int n = sc.nextInt();
 
-   System.out.println((int)(Math.pow(Math.sqrt(Math.pow(4, n)) + 1, 2)));
+   System.out.println((int)(Math.pow(Math.pow(2, n) + 1, 2)));
     }
   }  
 
